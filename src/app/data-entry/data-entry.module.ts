bc572bd645be94adf456fd013c33b17616a576e5
tusar2 <tusar@sdrc.co.in>
2018-04-04 18:55:38 +0530

    static page changes  #tusar

--- src/app/data-entry/data-entry.module.ts
+++ src/app/data-entry/data-entry.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DataEntrySelectionComponent } from './data-entry-selection/data-entry-selection.component';
 import { DataEntryHeadComponent } from './data-entry-head/data-entry-head.component';
-import { routing } from './data-entry.routing';
+//import { routing } from './data-entry.routing';
 
 @NgModule({
   imports: [
     CommonModule,
-    routing
+   // routing
   ],
   declarations: [DataEntrySelectionComponent, DataEntryHeadComponent],
   exports: [DataEntrySelectionComponent, DataEntryHeadComponent]
